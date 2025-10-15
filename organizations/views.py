d6a4e524b34ff9fa93196437916331d2bd470aa0
zeinu-dev <zeinusalah@gmail.com>
2025-10-15 20:14:03 +0300

    Updated views.py

--- organizations/views.py
+++ organizations/views.py
@@ -9,7 +9,7 @@ import json
 import traceback
 import logging
 from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
